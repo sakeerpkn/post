83f7947c7a15dfce4ad293e7abe86912ad52c322
sakeer <sakeerpkn@gmail.com>
2016-02-16 19:25:30 +0530

    created

--- manage.py
+++ manage.py
@@ -2,6 +2,9 @@
 #!/usr/bin/env python
 import os
 import sys
+a = 1
+if a == 1:
+    raise Exception("Script failed because of bla bla bla")
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
