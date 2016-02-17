90074c55596302fe4694d64cd5b66dcf4dd15569
sakeer <sakeerpkn@gmail.com>
2016-02-17 13:47:43 +0530

    exception has raised

--- manage.py
+++ manage.py
@@ -3,11 +3,12 @@
 import os
 import sys
 
-fffffff  if aa
-	jsooooo
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
+	try:
+	    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settingss")
 
-    from django.core.management import execute_from_command_line
+	    from django.core.management import execute_from_command_line
 
-    execute_from_command_line(sys.argv)
+	    execute_from_command_line(sys.argv)
+	except Exception as e:
+		raise Exception("Script failed because of failed to start the server and exception has raised"+ str(e))
